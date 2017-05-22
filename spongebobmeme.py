f6be2701f3ffdebc9e1dc82a50d8336b76dd45de
dasporal <dasporal@gmail.com>
2017-05-22 22:23:22 +0200

    shortened spongebobmeme.py

--- spongebobmeme.py
+++ spongebobmeme.py
@@ -1,7 +1,5 @@
 string = list(str(input('prepare your meme : ')))
 for index, i in enumerate(string):
-    if index%2 == 0:
-        string[index] = i
-    else:
+    if index%2 != 0:
         string[index] = i.upper()
 print("".join(string))
