ef67f5bcf16aa577750b6312616ffbf8bfe195fe
dasporal <dasporal@gmail.com>
2017-05-26 10:28:15 +0200

    so it's a wwebsite now

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Description
 
 In case you don't know what random means, it means that everything here is useless and doesn't deserve its own repo.
+
+~~It's available as a website here~~ Not yet.
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,16 @@
 
-window.onload = function(){
-    console.log('Hello World!');
-}
+$(document).ready(function () {
+    $('.modal').modal();
+    $('#spongebobConvert').click(function () {
+        spongebobMeme($('#spongebobInput').val(),$('#spongebobOutput'));
+    });
+});
+
+function spongebobMeme(input, output) {
+    var strOutput = "";
+    for(var i = 0; i<input.length;i++){
+        if(i%2!=0) strOutput= strOutput + input[i].toUpperCase();
+        else strOutput= strOutput + input[i];
+    }
+    output.html(strOutput);
+};
